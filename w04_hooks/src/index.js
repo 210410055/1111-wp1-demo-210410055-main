f580b63481a6eaf53f4944c952e1f71ca2e89ae3
210410055 <91882820+210410055@users.noreply.github.com>
2022-09-29 18:15:50 +0800

    Add files via upload

---
+++ w04_hooks/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App_55 from './App_55';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App_55 />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
